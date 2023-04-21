6d2fec2250a9907c5481a6649c8a24cf7c3a2618
hannah1306 <suhyun1306@gmail.com>
2023-04-21 12:07:52 +0900

    initial commit

---
+++ App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+function App() {
+
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Hello!</Text>
+      </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
